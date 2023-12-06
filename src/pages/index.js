ead64b56c1aa181990100fe4666a006f85846f45
QuanZzz <zhouquaneric@gmail.com>
2023-12-06 15:18:42 -0500

    add Typography component

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import { Typography } from "@/components/Typography"
+
+export default function Page() {
+  console.log("HELLO WORLD!");
+
+  return (
+    <Typography brand size='sm' bold>Hello World</Typography>
+  )
+}
